c15b82b4865567c8d358faf69575790fd66adfad
a-dornez <142704596+a-dornez@users.noreply.github.com>
2023-09-08 16:11:18 -0300

    feature: added class WelcomeMessage

---
+++ bank-account/src/WelcomeMessage.java
@@ -0,0 +1,11 @@
+public class WelcomeMessage {
+
+    public static void welcomeMessage () {
+
+        System.out.println("\nWelcome to Digital Innovation One financial solutions!\n");
+        System.out.println("We glad you decided to join us.");
+        System.out.println("Let us procede with your registration:\n");
+
+    }
+    
+}
